9cb07c23e487954dac576cd334b00ccb8ff13d71
DimEg <dmitry.egorov@2nova.ru>
2019-03-02 22:37:12 +0300

    Config heroky deploy

--- apps/id_90_web/config/prod.exs
+++ apps/id_90_web/config/prod.exs
@@ -17,7 +17,7 @@ config :id_90_web, Id90Web.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [scheme: "https", host: "pacific-meadow-71671.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # ## SSL Support
